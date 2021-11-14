83b0eaaaad4cf3173e32ccb2a4f457574774ffc1
Grapava00 <85571784+Grapava00@users.noreply.github.com>
2021-11-15 00:34:47 +0400

    task1(update)

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 //1.  წამოვიღოთ მომხმარებლების სია ამ მისამართიდან http://api.kesho.me/v1/user-test/index.
 
-const getUsers = fetch('http://api.kesho.me/v1/user-test/index')
-    .then(resp => {
-        return resp.json();
-    })
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.log(error)
-    })
+function getUsers() {
+    fetch('http://api.kesho.me/v1/user-test/index')
+        .then(resp => {
+            return resp.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+getUsers();
 
 
-//2. შევქმნათ მომხმარებლების ცხრილი (ცხრილი.png).
+//2. შევქმნათ მომხმარებლების ცხრილი (ცხრილი.png)
+
+
 
